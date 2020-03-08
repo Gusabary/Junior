b89903ee7e77d1c8ae974767521d00bd4da1da3c
unknown <Gusabary@126.com>
2020-03-08 18:44:50 +0800

    feat(gp): finish homework 1

---
+++ GP/homework-1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    private int count;
+    private float endTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+        endTime = -1;
+        setCountText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        GetComponent<Rigidbody>().AddForce(movement * speed * Time.deltaTime);
+
+        if (endTime >= 0) {
+            endTime += Time.deltaTime;
+        }
+        if (endTime > 1) {
+            Application.Quit();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "PickUp") {
+            other.gameObject.SetActive(false);
+            count++;
+            setCountText();
+        }
+    }
+
+    void setCountText() {
+        if (count == 8) {
+            countText.text = "Complete!";
+            endTime = 0;
+        }
+        else {
+            countText.text = "Count: " + count;
+        }
+    }
+}
