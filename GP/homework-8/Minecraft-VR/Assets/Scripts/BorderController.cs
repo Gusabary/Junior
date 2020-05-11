e7ee1b493020c79ad0e060220d5425c0b64e6366
unknown <Gusabary@126.com>
2020-05-11 17:35:35 +0800

    feat(gp): finish homework 8

---
+++ GP/homework-8/Minecraft-VR/Assets/Scripts/BorderController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BorderController : MonoBehaviour
+{
+    public Vector3 dir;
+    public GameObject player;
+    float mainSpeed = 0.5f; //regular speed
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Move() {
+        player.transform.position += dir * mainSpeed;
+    }
+}
---
+++ GP/homework-8/Minecraft-VR/Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    private int score;
+    public Text scoreNum;
+    public GameObject gameStartPanel;
+    public GameObject gameOverPanel;
+
+    private void Awake() {
+        if (instance != null) {
+            Destroy(gameObject);
+        }
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance.score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public static void AddScore() {
+        instance.score += 10;
+        instance.scoreNum.text = instance.score.ToString("000");
+    }
+
+    public static void GameOver() {
+        instance.gameOverPanel.SetActive(true);
+    }
+
+    public void RestartGame() {
+        instance.score = 0;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartGame() {
+        instance.gameStartPanel.SetActive(false);
+    }
+}
